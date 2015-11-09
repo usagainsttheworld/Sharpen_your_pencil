f5e79ae03d94bc1ae931cac71478817a90e782fd
Ling Tian <sweetbellor.ling@gmail.com>
2015-11-09 10:11:41 -0500

    instance variables, private, public

--- src/Puzzle4.java
+++ src/Puzzle4.java
@@ -4,31 +4,31 @@
 
 public class Puzzle4 {
     public static void main (String[] args) {
-        Puzzle4[] obs = new Puzzle4[6];
+        Puzzle4b[] obs = new Puzzle4b[6];
         int y = 1;
         int x = 0;
         int result = 0;
         while (x < 6) {
-            xxx;
-            xxx;
+            obs[x] = new Puzzle4b();
+            obs[x].ivar = y;
             y = y * 10;
-            xxxx;
+            x = x + 1;
         }
         x = 6;
         while (x > 0) {
-            xxx;
-            result = result + xxxx;
+            x = x - 1;
+            result = result + obs[x].doStuff(x);
         }
         System.out.println("result " + result);
     }
 }
-class Puzzle4b{
+class Puzzle4b {
     int ivar;
-    obs[x].doStuff (int factor) {
+    public int doStuff (int factor) {
         if (ivar > 100) {
-            return xxx;
+            return ivar * factor;
         } else {
-            return xxx;
+            return ivar * (5 - factor);
         }
     }
 }
